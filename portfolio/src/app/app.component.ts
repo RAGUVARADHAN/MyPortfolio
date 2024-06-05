82c90b806abe1993531802b0cc1b652382a6b326
RAGUVARADHAN <raguvaradhansv@gmail.com>
2024-06-05 14:06:01 +0530

    modify in mywork

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,22 @@ export class AppComponent {
   scroll:boolean=false;
 
   img="/assets/images/home_img.jpg";
+  constructor(private activateroute:ActivatedRoute){}
+
+  ngOnInit()
+  {
+    this.activateroute.fragment.subscribe(value=>
+      {
+        console.log(value);
+        this.jumpTo(value);
+      }
+    )
+  }
+
+  jumpTo(section:any)
+  {
+    document.getElementById(section)?.scrollIntoView({behavior:'smooth'});
+  }
 
   @HostListener('window:scroll',['$event'])
   Onscroll(event:any)
